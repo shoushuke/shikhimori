6fa69fbed72500d2d9ab56a626d4c9ccd2d09897
shoushuke <134142452+shoushuke@users.noreply.github.com>
2023-05-21 15:05:15 +0530

    Update config.py

--- shikimori/config.py
+++ shikimori/config.py
@@ -1,36 +1,43 @@
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
 
-from Shikimori.vars import HEROKU_API_KEY, HEROKU_APP_NAME, REDIS_URL
 
 def get_user_list(config, key):
-    with open("{}/Senku/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/EnmuBot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
-    # REQUIRED
-    # Login to https://my.telegram.org and fill in these slots with the details given by it
-
-    API_ID = 20845525  # integer value, dont use ""
+    TEMP_DOWNLOAD_DIRECTORY = "/tmp"
+    API_ID = 20845525
     API_HASH = "0050453e7ad58679e823fa5767117ff7"
-    BOT_TOKEN = "5918398685:AAESsrTJ0YwIASlYIe7u-UZbWWmVIIE8CKA"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1981344911  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "shoushuke"
-    SUPPORT_CHAT = "humans_are_here"  # Your own group for support, do not add the @
-    LOG_CHANNEL = (
-        -1504590700
+    ARQ_API = "RVLOWH-KMDHXW-QQCPAC-EFICND-ARQ"
+    TOKEN = "5918398685:AAESsrTJ0YwIASlYIe7u-UZbWWmVIIE8CKA"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    BOT_ID = 5918398685
+    OWNER_ID = 2005266280  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Lost_Emotion"
+    SUPPORT_CHAT = "Enmu_chat_Support"  # Your own group for support, do not add the @
+    BOT_USERNAME = "@shikimori_noobbot"
+    JOIN_LOGGER = (
+        -1001774850421
+    )  # Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = (
+        -1001774850421
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-
+    ERROR_LOGS = (
+        -1001711427851
+    )  # Prints information Error
+    REDIS_URL = "redis://default:cdgiOMJGyZsqx97p5S7S@containers-us-west-27.railway.app:6644"
+    MONGO_DB_URI = "mongodb+srv://enmu:enmu123@enmu.2cuev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    DATABASE_URL = "postgresql://postgres:nR1yc3HQjFJSJcXBy4V3@containers-us-west-33.railway.app:7793/railway"  # needed for any database modules
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
+    INFOPIC = True
     URL = None
+    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
@@ -43,6 +50,7 @@ class Config(object):
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     TIGERS = get_user_list("elevated_users.json", "tigers")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
+    DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
@@ -50,37 +58,20 @@ class Config(object):
     WORKERS = (
         8  # Number of subthreads to use. Set as number of threads your processor uses
     )
+    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
         "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = "-0X68BOFAQTLJ"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
-        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
+        "6950f559377140a4e1594c564cdca6a3"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
-    AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
-    ERROR_LOG_CHANNEL = -1001501815938  # needed to make sure 'save from' messages persist
-    HEROKU_API_KEY = 2088106582  # Your Heroku API key, get it from 'https://dashboard.heroku.com/account
-    HEROKU_APP_NAME = (
-        "awoo"  # Enter the Heroku app name here (Must an exact same name with your input above)
-    )
-    ARQ_API = "FCGUBP-TMPNAX-APQOHT-GQKNVU-ARQ"
-    APOD_API_KEY = "IeulgCzxQ2WfdCDtfXsaMbSGnJTFqNM8wPsncBN6"
-    REDIS_URL = "redis://default:cdgiOMJGyZsqx97p5S7S@containers-us-west-27.railway.app:6644"
-    ANIME_NAME = "Shikimori's not just a cutie"
-    START_MEDIA = "https://telegra.ph/file/9235d57807362b4e227a3.mp4"
-    BOT_USERNAME = "shikimori_noobbot"
-    UPDATE_CHANNEL = "knights-x-association"
-    ALIVE_MEDIA = "https://telegra.ph/file/2b04f7812f22b983f8a10.mp4"
-    BOT_ID = 5918398685
-    STATS_IMG = ""
-    NETWORK_USERNAME = "knights-x-association"
-    NETWORK = "knights network"
-    INLINE_IMG = "https://telegra.ph/file/8cec66d01df8c0071ebaa.jpg"
-    API_WEATHER = ""
-    OWNER_WELCOME_MEDIA = ""
+    REM_BG_API_KEY = "-QcoQXCAXyhAxeLN2eD86LeFa"
+    OPENWEATHERMAP_ID = "887da2c60d9f13fe78b0f9d0c5cbaade"
+
 
 class Production(Config):
     LOGGER = True
