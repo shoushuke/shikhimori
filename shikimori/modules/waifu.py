cbdd9a5e3aa86448b3d20f3df8651c1224bc3153
shoushuke <134142452+shoushuke@users.noreply.github.com>
2023-06-08 10:32:59 +0530

    Update waifu.py

--- shikimori/modules/waifu.py
+++ shikimori/modules/waifu.py
@@ -1,28 +1,4 @@
-"""
-STATUS: Code is working. ✅
-"""
 
-"""
-GNU General Public License v3.0
-
-Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
-# Created By: https://telegram.dog/SOME1_HING 
-# Assisted By: https://telegram.dog/SastaDev from https://telegram.dog/SastaNetwork
 
 # Imports from external libraries. (DON'T EDIT)
 import requests
