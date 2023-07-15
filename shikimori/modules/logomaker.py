1b8da4692746d3fb423b5ebc461d3d6b3a13bb55
shoushuke <134142452+shoushuke@users.noreply.github.com>
2023-07-15 19:27:51 +0530

    Update logomaker.py

--- shikimori/modules/logomaker.py
+++ shikimori/modules/logomaker.py
@@ -1,29 +1,3 @@
-"""
-STATUS: Code is working. ✅
-"""
-
-"""
-GNU General Public License v3.0
-
-Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
-
-Credits:-
-    Mizuhara Team
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 from Shikimori.events import register
 from Shikimori import OWNER_ID, dispatcher
 from Shikimori.vars import BOT_USERNAME, SUPPORT_CHAT
